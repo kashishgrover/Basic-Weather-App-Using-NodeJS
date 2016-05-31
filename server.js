8196bb7488ca5718d8f940e527aac3544a65ce4c
Kashish Grover <kashishgrover95@gmail.com>
2016-05-31 13:21:10 +0530

    Branch 1 First Commit

--- server.js
+++ server.js
@@ -9,18 +9,22 @@ var async = require('async');
 
 var app = express();
 
-var lines = {};
-var message = "";
-var json="";
-
 var data = "";
 var str = "";
-var name = "";
-var lat = "";
-var long = "";
-var temperature = "";
-var observationtime = "";
-var skytext = ""; 
+
+var name1 = "";
+var lat1 = "";
+var long1 = "";
+var temperature1 = "";
+var observationtime1 = "";
+var skytext1 = ""; 
+
+var name2 = "";
+var lat2 = "";
+var long2 = "";
+var temperature2 = "";
+var observationtime2 = "";
+var skytext2 = ""; 
 
 // set the view engine to ejs
 app.set('view engine', 'ejs');
@@ -29,14 +33,21 @@ app.set('view engine', 'ejs');
 
 // index page 
 app.get('/', function(req, res) {
-	res.locals.name = req.name;
+	res.locals.name1 = req.name1;
+	res.locals.name2 = req.name2;
     res.render('pages/index', {
-   				name: name,
-    	  		lat: lat,
-	    		long: long,
-   				temperature: temperature,
-   				observationtime: observationtime,
-  				skytext: skytext
+   				name1: name1,
+    	  		lat1: lat1,
+	    		long1: long1,
+   				temperature1: temperature1,
+   				observationtime1: observationtime1,
+  				skytext1: skytext1,
+  				name2: name2,
+    	  		lat2: lat2,
+	    		long2: long2,
+   				temperature2: temperature2,
+   				observationtime2: observationtime2,
+  				skytext2: skytext2
 			});
 });
 
@@ -46,57 +57,82 @@ app.get('/about', function(req, res) {
 });
 
 app.post('/', function (req, res) {
-	json="";
-    message = "";
+
     processFormFieldsIndividual(req, res);
+
 });
 
 function processFormFieldsIndividual(req, res) {
 
-    var city = "";
+    var city1 = "";
+    var city2 = "";
 
     var fields = [];
     
     var form = new formidable.IncomingForm();
     
-    form.parse(req, function(err, fields, files) {
-    	    	
-    	//console.log(fields);        
-        city = fields.city;
+    form.parse(req, function(err, fields) {
 
-        weather.find({search: city, degreeType: 'C'}, function(err, result) {  			
-  			if(err) console.log(err); 
-  			else{
-  				data = result[0];
-  				//console.log("1");
-  				//console.log('Data (67): ', data);
+        city1 = fields.city1;
+        city2 = fields.city2;
 
-	  			name = data.location.name;
-  				console.log("71: "+name);
-  				lat = data.location.lat;
-  				console.log("73: "+lat);
-  				long = data.location.long;
-  				console.log("75: "+long);
-  				temperature = data.current.temperature+" *"+data.location.degreetype;
-  				console.log("77: "+temperature);
-  				skytext = data.current.skytext;	
-  				console.log("79: "+skytext);
-  				observationtime = data.current.observationtime;
-  				console.log("81: "+observationtime);
-  				//console.log('Name (73): ', name);
-  				res.render('pages/index', {
-   					name: name,
-    	  			lat: lat,
-	    			long: long,
-   					temperature: temperature,
-   					observationtime: observationtime,
-  					skytext: skytext
-				}); 		
-  			} 			
+        console.log("City 1: ",city1);
+        console.log("City 2: ",city2);
+
+       	runTwoWeatherFinds(err, res);       
+
+		res.render('pages/index', {
+   			name1: name1,
+    		lat1: lat1,
+	    	long1: long1,
+   			temperature1: temperature1,
+   			observationtime1: observationtime1,
+  			skytext1: skytext1,
+  			name2: name2,
+    		lat2: lat2,
+	    	long2: long2,
+   			temperature2: temperature2,
+   			observationtime2: observationtime2,
+  			skytext2: skytext2
 		});
-
     });
 
+function runTwoWeatherFinds(err, res)
+{
+	if(err) console.log(err);
+	else
+	{
+		console.log('1');
+		weather.find({search: city1, degreeType: 'C'}, function(err, result) {
+        	if(err) console.log(err); 
+  			else {
+  				data1 = result[0];
+  				console.log(data1);
+  				name1 = data.location.name;
+  				lat1 = data.location.lat;
+  				long1 = data.location.long;
+  				temperature1 = data.current.temperature+" *"+data.location.degreetype;
+  				skytext1 = data.current.skytext;	
+  				observationtime1 = data.current.observationtime;
+  			}
+        });
+		console.log('2');
+        weather.find({search: city2, degreeType: 'C'}, function(err, result) {  			
+  			if(err) console.log(err); 
+  			else {
+  				data2 = result[0];
+  				console.log(data2);
+	  			name2 = data.location.name;
+  				lat2 = data.location.lat;
+  				long2 = data.location.long;
+  				temperature2 = data.current.temperature+" *"+data.location.degreetype;
+  				skytext2 = data.current.skytext;
+  				observationtime2 = data.current.observationtime;	
+  			}
+		});
+		console.log('3');
+	}	 
+}
     form.parse(req);
 }
 
